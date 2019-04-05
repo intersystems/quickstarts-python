a1b66ee6f7535a4afe81a580f31736f75a37d20b
DuongDo-InterSystems <Duong.Do@intersystems.com>
2019-04-05 11:54:54 -0400

    Add code for Python QuickStarts

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # quickstarts-python
-Sample repo for Python QuickStart
+This code shows PyODBC, and the Native API access. It is required for the Python quickstart which can be found here: https://learning.intersystems.com/course/view.php?name=Python%20QS   
+Note that our code and supporting wheel files are designed for Python 3 because Python 2 will retire in 2020. If you want to use Python 2, please contact us for more details.
+
+
+## Contents
+* `pyodbcplayTask7.py`: to see how to store and retrieve data relationally
+* `nativeplayTask4.py`: to see how to run methods within InterSystems IRIS
+## Configuration files
+`connections.config`: contains connection details for PyODBC, and Native API.
+
+## Connect to InterSystems via PyODBC
+
+**Note**: If you have InterSystems IRIS installed locally in your machine, you do not need to set up InterSystems PyODBC driver.
+
+**Prerequisite**: You should have [PyODBC](https://pypi.org/project/pyodbc/) already installed in your local machine.
+If you having trouble installing PyODBC, please see [how to install PyODBC](pyodbc_install.md)
+
+### Set up InterSystems PyODBC driver for Windows:
+
+* Execute `ODBC-2018.1.1.635.0-win_x64.exe` file.
+
+###  Set up InterSystems PyODBC driver for Mac OSX and Linux:
+
+* Run `odbcinst –i –d –f odbcinst.ini`
+
+## Connect to InterSystems via the Native API
+
+### Set up the Native API for Windows:
+
+* Run `pip install wheel/irisnative-1.0.0-cp36-cp36m-win_amd64.whl`
+
+**Note:** Depending on the version of Python 3 and the version of Platform that you are using, you may have to change the filename to get the wheel file to be installed.
+
+* If you are using Python 3.7, rename `irisnative-1.0.0-cp36-cp36m-win_amd64.whl` to `irisnative-1.0.0-cp37-cp37m-win_amd64.whl`
+
+### Set up the Native API for Mac:
+
+* Run `pip install wheel/irisnative-1.0.0-cp34-abi3-macosx_10_13_x86_64.macosx_10_14_x86_64.whl`
+
+### Set up the Native API for Linux:
+
+* Run `pip install wheel/irisnative-1.0.0-cp34-abi3-linux_x86_64.whl`
+
+## How to run
+
+1. Clone the repo and open it in your IDE.
+2. In `connections.config` file, modify the host and password to be the correct values for your InterSystems IRIS instance. 
+Although port and username are most likely the defaults, you should verify those values are corrects.
+3. To run a file, in the terminal, type: `python filename.py`, in which, replace `filename.py` with the name of the file you want to run.
---
+++ nativeplayTask2.py
@@ -0,0 +1,108 @@
+# PURPOSE: Store stock data directly to InterSystems IRIS Data Platform using a custom structure.
+#
+# NOTES: When running, choose option 2 to store stock data natively.
+
+import irisnative
+from time import time
+
+
+# Write to a test global
+def set_test_global(iris_native):
+    iris_native.set(8888, "^testglobal", "1")
+    global_value = iris_native.get("^testglobal", "1")
+    print("The value of ^testglobal(1) is {}".format(global_value))
+
+
+# Store stock data directly into InterSystems IRIS
+def store_stock_data(iris_native):
+    # Clear global from previous runs
+    iris_native.kill("^nyse")
+    print("Storing stock data using Native API...")
+
+    # Get stock data from file
+    list_stock = []
+    with open("all_stocks.csv") as f:
+        lines = f.readlines()
+
+        # Add stock data to list
+        for line in lines:
+            list_stock.append(line.rstrip("\n"))
+
+    # Get current time
+    start = int(time() * 1000)
+
+    # Loop through list of stock and write global
+    for i in range(len(list_stock)):
+        iris_native.set(list_stock[i], "^nyse", i + 1)
+
+    # Get time consuming
+    end = int(time() * 1000)
+    time_consume = end - start
+    print("Stored natively successfully. Execution time: {} ms".format(time_consume))
+
+
+# Execute task based on user input
+def execute_selection(selection, iris_native):
+    if selection == 1:
+        set_test_global(iris_native)
+    elif selection == 2:
+        store_stock_data(iris_native)
+    elif selection == 3:
+        print("TO DO: View stock data")
+    elif selection == 4:
+        print("TO DO: Generate trades")
+
+
+# Get connection details from config file
+def get_connection_info(file_name):
+    # Initial empty dictionary to store connection details
+    connections = {}
+
+    # Open config file to get connection info
+    with open(file_name) as f:
+        lines = f.readlines()
+        for line in lines:
+            # remove all white space (space, tab, new line)
+            line = ''.join(line.split())
+
+            # get connection info
+            connection_param, connection_value = line.split(":")
+            connections[connection_param] = connection_value
+    return connections
+
+
+def run():
+    # Retrieve connection information from configuration file
+    connection_detail = get_connection_info("connection.config")
+
+    ip = connection_detail["ip"]
+    port = int(connection_detail["port"])
+    namespace = connection_detail["namespace"]
+    username = connection_detail["username"]
+    password = connection_detail["password"]
+
+    # Create connection to InterSystems IRIS
+    connection = irisnative.createConnection(ip, port, namespace, username, password)
+    print("Connect to InterSystem IRIS")
+
+    # Create IRIS Native object
+    iris_native = irisnative.createIris(connection)
+
+    # Starting interactive prompt
+    while True:
+        print("1. Test")
+        print("2. Store stock data")
+        print("3. View stock data")
+        print("4. Generate Trades")
+        print("5. Quit")
+        selection = int(input("What would you like to do? "))
+        if selection == 5:
+            break
+        elif selection not in range(1, 6):
+            print("Invalid option. Try again!")
+            continue
+        execute_selection(selection, iris_native)
+
+
+if __name__ == '__main__':
+    run()
